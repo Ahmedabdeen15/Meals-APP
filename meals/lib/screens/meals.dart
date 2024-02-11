7e63514dafed07c856399342d91a67c6457bc257
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-02-11 02:09:12 +0200

    adding function to reach meal details screen

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealScreen extends StatelessWidget {
   const MealScreen({super.key, required this.title, required this.meals});
   final String title;
   final List<Meal> meals;
+  void selectedMealDetails(BuildContext context, Meal meal) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+              meal: meal,
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
+        itemBuilder: (ctx, index) => MealItem(
+              meal: meals[index],
+              onSelectMealDetails: () {
+                selectedMealDetails(context, meals[index]);
+              },
+            ));
     if (meals.isEmpty) {
       content = Center(
         child: Column(
